3bb6b4c11f070c6c0fd04bb14ad70b36113c2c30
abehrensFDU <a.behrens@student.fdu.edu>
2025-09-13 19:24:21 -0400

    BAAD-6 Added documentation on interfaces and scope

--- BAAD-TicTacToe/BAAD-TicTacToe.cpp
+++ BAAD-TicTacToe/BAAD-TicTacToe.cpp
@@ -12,4 +12,55 @@ int main()
 	Board board;
 	board.printBoard();
 
+
+	/*
+	main
+keeps track of whose turn (just loop through)
+keeps track of if win or if draw
+Instructions listed will say x always goes first, etc
+	
+	Use case
+	Prompt player to enter move
+	<figure out player's turn>
+		int array playermove = playerx->get move()
+		board->setBoxIcon(playermove[0], playermove[1], playerx.geticon()
+			board->printBoard()
+			board->checkWin()
+			if win(ask for reset)
+				board->checkDraw()
+				if draw(ask for reset)
+
+
+					playermove[1], playerX.geticon())
+		playermove = playero->get move()
+		board->setBoxIcon(playermove[0], playermove[1], playero.geticon)
+		board->printBoard()
+		board->checkWin()
+		if win(ask for reset)
+			board->checkDraw()
+			if draw(ask for reset) 
+			
+			
+			player
+			X and O
+ int array getMove()
+int geticon()
+player constructor stores icon
+
+
+
+Board
+reset function
+3*3 grid
+bool checkWin()
+bool checkDraw()
+(turns counter to check for draw)
+
+We are gonna have to make cells that is gonna hold the value
+ Could be either X OR O OR  ' '
+
+			*/
+
+
+
 }
