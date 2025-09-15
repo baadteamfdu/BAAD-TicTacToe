53b36e4ff382ca543300603c722b04e2a6375ded
HP <HP@DESKTOP-09GEG5C>
2025-09-15 18:53:04 -0400

    BAAD-13: I made some changes in the printboard to make the board look visually better. wrote the checkWin function. set the function type of "setBoxIcon" from void to  bool.

--- BAAD-TicTacToe/Board.cpp
+++ BAAD-TicTacToe/Board.cpp
@@ -2,43 +2,78 @@
 #include <iostream>
 using namespace std;
 
-void Board::reset()
-{
+Board::Board() {
+    reset();
+}
+
+void Board::reset() {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             boardArray[i][j] = ' ';
         }
     }
-	turns = 0;
+    turns = 0;
 }
 
-void Board::printBoard()
-{
+void Board::printBoard() {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             cout << " " << boardArray[i][j];
-            cout << "|";
+            if (j < 2) cout << " |";
         }
         cout << endl;
-        cout << "-- -- --";
+        if (i < 2) cout << "---+---+---";
         cout << endl;
     }
+    cout << endl;
 }
 
 bool Board::checkDraw() {
     if (turns >= 9) {
-        return true;        //might need changing based on the implementation of others to be 8 or 10
+        return true;        
     }
-    else {
+    else 
+    {
         return false;
     }
+}
+
+
+bool Board::checkWin(char player) {
+    
+    for (int i = 0; i < 3; i++) {
+        if (boardArray[i][0] == player &&
+            boardArray[i][1] == player &&
+            boardArray[i][2] == player) return true;
+
+        if (boardArray[0][i] == player &&
+            boardArray[1][i] == player &&
+            boardArray[2][i] == player) return true;
+    }
+
+    // or diagonal
+    if (boardArray[0][0] == player &&
+        boardArray[1][1] == player &&
+        boardArray[2][2] == player) return true;
+
+    if (boardArray[0][2] == player &&
+        boardArray[1][1] == player &&
+        boardArray[2][0] == player) return true;
+
+    return false;
 }
 
 char Board::getBoxIcon(int row, int col) {
     return boardArray[row - 1][col - 1];
 }
 
-void Board::setBoxIcon(int row, int col, char icon) {
-    boardArray[row-1][col-1] = icon;
-    turns++;
+bool Board::setBoxIcon(int row, int col, char icon) {
+    if (row >= 1 && row <= 3 && col >= 1 && col <= 3) {
+        if (boardArray[row - 1][col - 1] == ' ') {
+            boardArray[row - 1][col - 1] = icon;
+            turns++;
+            return true;
+        }
+    }
+    return false; 
 }
--- BAAD-TicTacToe/Board.h
+++ BAAD-TicTacToe/Board.h
@@ -2,18 +2,19 @@
 using namespace std;
 
 class Board {
-    public:
+private:
+    int turns;
+    char boardArray[3][3];
+ public:
+     Board();
         void reset();
-        bool checkWin();
-        bool checkDraw();
-        char getBoxIcon(int row, int col);
-        void setBoxIcon(int row, int col, char icon);
         void printBoard();
-    private:
-        int turns;
-        char boardArray[3][3] = { ' ', ' ', ' ',
-                                  ' ', ' ', ' ',
-                                  ' ', ' ', ' ' };
+        bool checkDraw();
+        bool checkWin(char player);
+        char getBoxIcon(int row, int col);
+        bool setBoxIcon(int row, int col, char icon);
+        
+ 
 
 };
 
