1a79190710cf2b9a1ed3cb59bcedb1d215b0a608
abehrensFDU <a.behrens@student.fdu.edu>
2025-09-15 14:20:03 -0400

    BAAD-11 Added checking for alphanumeric characters / not number input, imported string which I hope is allowed.

--- BAAD-TicTacToe/Player.cpp
+++ BAAD-TicTacToe/Player.cpp
@@ -1,4 +1,5 @@
 #include "Player.h"
+#include <string>
 #include <iostream>
 using namespace std;
 
@@ -11,23 +12,46 @@ char Player::getPlayerIcon()
 	return icon;
 }
 
-int* Player::getMove()
-{
+int* Player::getMove() {
 	int* move = new int[2];
+	string input = "";
+	int moveValid;
 	do {
+		moveValid = 0;
 		cout << "What will your row be (1 to 3)? " << endl;
-		cin >> move[0];
-		if (move[0] < 1 || move[0] > 3) {
-			cout << "ERROR ::: ROW DOES NOT EXIST. PLEASE CHOOSE ROW FROM 1 TO 3" << endl;
+		cin >> input;
+		try {
+			moveValid = stoi(input);
+			if (moveValid < 1 || moveValid > 3) {
+				cout << "ERROR ::: ROW DOES NOT EXIST. PLEASE CHOOSE ROW FROM 1 TO 3" << endl;
+			}
+			else {
+				move[0] = moveValid;
+			}
 		}
-	} while (move[0] < 1 || move[0] > 3);
-	do {
-		cout << "What will your column be (1 to 3)? " << endl;
-		cin >> move[1];
-		if (move[1] < 1 || move[1] > 3) {
-			cout << "ERROR ::: ROW DOES NOT EXIST. PLEASE CHOOSE ROW FROM 1 TO 3" << endl;
+		catch (exception& e) {
+			cout << "ERROR ::: INVALID INPUT. PLEASE CHOOSE ROW FROM 1 TO 3" << endl;
 		}
-	} while (move[1] < 1 || move[1] > 3);
+	} while (moveValid < 1 || moveValid > 3);
 
-	return move;
-}
+	do {
+		moveValid = 0;
+		cout << "What will your column be (1 to 3)? " << endl;
+		cin >> input;
+		try {
+			moveValid = stoi(input);
+			if (moveValid < 1 || moveValid > 3) {
+				cout << "ERROR ::: COLUMN DOES NOT EXIST. PLEASE CHOOSE COLUMN FROM 1 TO 3" << endl;
+			}
+			else {
+				move[1] = moveValid;
+			}
+		}
+		catch (exception& e) {
+			cout << "ERROR ::: INVALID INPUT. PLEASE CHOOSE COLUMN FROM 1 TO 3" << endl;
+		}
+	}
+		while (moveValid < 1 || moveValid > 3);
+
+		return move;
+	}
