12c026140e72fb1f712197e7fbc295116126fc36
daved1173 <daved1173@gmail.com>
2025-09-16 07:44:10 -0400

    Update BAAD-TicTacToe.cpp
    "Needs to loop for full game and check for win and draw, then give option to restart and quit."
    
    Took these notes into consideration when redoing the code. Looped the full game, made the code so that it would check for wins and draws, and finally it gives the option to either restart or quit.

--- BAAD-TicTacToe/BAAD-TicTacToe.cpp
+++ BAAD-TicTacToe/BAAD-TicTacToe.cpp
@@ -1,95 +1,90 @@
 #include <iostream>
+#include <cctype>
 #include "Board.h"
 #include "Player.h"
 using namespace std;
 
-int main()
+bool askRestart() 
 {
-	cout << "Instructions: " << endl;
-	cout << "The player going first uses 'x', and the second uses 'o'" << endl;
-	cout << "When it is your turn, enter which ROW and COLUMN you would like to select(numbers 1-3)" << endl;
-	cout << "You CANNOT play on a spot that is already taken, otherwise you will be asked again!" << endl;
-	cout << "First to 3 in a row wins! If the board has no winner, it is a draw!" << endl;
-	cout << "Loser goes first if you play again! Good luck!" << endl;
-
-	Board board;
-	board.printBoard();
-    Player player1;
-    Player player2;
-    player1.setIcon('x');
-    player2.setIcon('o');
-
-	cout << "Player 1 " << player1.getPlayerIcon() << "'s turn:" << endl;
-	int* move1 = player1.getMove();
-	while (board.getBoxIcon(move1[0], move1[1]) != ' ')
+    while (true) 
 	{
-		cout << "ERROR ::: BOARD SPOT TAKEN. Try again.";
-		delete[] move1;
-		move1 = player1.getMove();
-	}
-	board.setBoxIcon(move1[0], move1[1], player1.getPlayerIcon());
-	delete[] move1;
-
-	board.printBoard();
-
-	cout << "Player 2 " << player1.getPlayerIcon() << "'s turn:" << endl;
-	int* move2 = player2.getMove();
-	while (board.getBoxIcon(move2[0], move2[1]) != ' ')
-	{
-		cout << "ERROR ::: BOARD SPOT TAKEN. Try again.";
-		delete[] move2;
-		move2 = player2.getMove();
-	}
-	board.setBoxIcon(move2[0], move2[1], player2.getPlayerIcon());
-	delete[] move2;
-
-	board.printBoard();
+        cout << "Play again? (r = restart, q = quit): ";
+        string s;
+        if (!getline(cin, s)) return false; // EOF -> quit
+        if (!s.empty()) 
+		{
+            char ch = static_cast<char>(tolower(static_cast<unsigned char>(s[0])));
+            if (ch == 'r') return true;
+            if (ch == 'q') return false;
+        }
+        cout << "Please enter 'r' or 'q'.\n";
+    }
 }
 
-	/*
-	main
-keeps track of whose turn (just loop through)
-keeps track of if win or if draw
-Instructions listed will say x always goes first, etc
-	
-	Use case
-	Prompt player to enter move
-	<figure out player's turn>
-		int array playermove = playerx->get move()
-		board->setBoxIcon(playermove[0], playermove[1], playerx.geticon()
-			board->printBoard()
-			board->checkWin()
-			if win(ask for reset)
-				board->checkDraw()
-				if draw(ask for reset)
+int main() 
+{
+    cout << "Instructions: " << endl;
+    cout << " - The player going first uses 'x', and the second uses 'o'" << endl;
+    cout << " - On your turn, enter ROW and COLUMN (numbers 1-3)" << endl;
+    cout << " - You CANNOT play on a taken spot; you'll be asked again" << endl;
+    cout << " - First to 3 in a row wins! If no winner and the board is full, it's a draw" << endl;
+    cout << " - Loser goes first if you play again. Good luck!" << endl;
 
+    Board board;
+    Player player1; player1.setIcon('x');
+    Player player2; player2.setIcon('o');
 
-					playermove[1], playerX.geticon())
-		playermove = playero->get move()
-		board->setBoxIcon(playermove[0], playermove[1], playero.geticon)
-		board->printBoard()
-		board->checkWin()
-		if win(ask for reset)
-			board->checkDraw()
-			if draw(ask for reset) 
-			
-			
-			player
-			X and O
- int array getMove()
-int geticon()
-player constructor stores icon
+    // starter: 0 -> player1 (x), 1 -> player2 (o)
+    int starter = 0;
 
+    bool keepPlaying = true;
+    while (keepPlaying) 
+	{
+        board.reset();
+        board.printBoard();
 
+        // Current player index for this round (0 or 1)
+        int turnIdx = starter;
 
-Board
-reset function
-3*3 grid
-bool checkWin()
-bool checkDraw()
-(turns counter to check for draw)
+        while (true) 
+		{
+            Player& cur = (turnIdx == 0) ? player1 : player2;
 
-We are gonna have to make cells that is gonna hold the value
- Could be either X OR O OR  ' '
+            cout << "Player " << (turnIdx + 1) << " (" << cur.getPlayerIcon() << ")'s turn:\n";
 
-			*/
+            int* mv = cur.getMove(); // expects 0-based [row, col] from your Player.h
+            while (board.getBoxIcon(mv[0], mv[1]) != ' ') 
+			{
+                cout << "ERROR ::: BOARD SPOT TAKEN. Try again.\n";
+                delete[] mv;
+                mv = cur.getMove();
+            }
+
+            board.setBoxIcon(mv[0], mv[1], cur.getPlayerIcon());
+            delete[] mv;
+
+            board.printBoard();
+
+            if (board.checkWin()) 
+			{
+                cout << "Player " << (turnIdx + 1) << " (" << cur.getPlayerIcon() << ") WINS!\n";
+                starter = (turnIdx == 0) ? 1 : 0;
+                keepPlaying = askRestart();
+                break;
+            }
+
+            if (board.checkDraw()) 
+			{
+                cout << "It's a DRAW!\n";
+                starter = 1 - starter;
+                keepPlaying = askRestart();
+                break;
+            }
+
+            turnIdx = 1 - turnIdx;
+        }
+    }
+
+    cout << "Thanks for playing!\n";
+    return 0;
+}
